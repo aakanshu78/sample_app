000f2e7e8727561124774f679a510bacf49f13b5
aakanshu <aakanshu78@gmail.com>
2017-01-03 15:17:55 +0530

    Adding static pages

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -16,11 +16,11 @@ def secure_token
 	token_file = Rails.root.join('.secret')
 	if File.exist?(token_file)
 		# Use the existing token
-		File.read(token-file).chomp
+		File.read(token_file).chomp
 
 	else
 		# Generate a new token and store it in token_file
-		token = SecureRandommm.hex(64)
+		token = SecureRandom.hex(64)
 		File.write(token_file, token)
 		token
 	end

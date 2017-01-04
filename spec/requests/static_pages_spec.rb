ac3944ab1d9d9041c27c841254e38ee2bedef808
aakanshu <aakanshu78@gmail.com>
2017-01-04 10:53:14 +0530

    Add a full_title helper

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe "Static pages" do
+  describe "Home page" do
+    it "works! (now write some real specs)" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      get static_pages_index_path
+      response.status.should be(200)
+    end
+  end
+
+  describe "Help page" do
+    it "should have the content'Help'" do
+      visit '/static_pages/help'
+      expect(pages).to have_content('Help')
+    end
+  end
+  
+end
